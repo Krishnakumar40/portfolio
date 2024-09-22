303bac71e98685a76edd5e497c0b5917e9a91fe9
Krishnakumar40 <94290165+Krishnakumar40@users.noreply.github.com>
2024-09-22 15:08:39 +0530

    deploy check

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -61,9 +61,8 @@ function Hero() {
             </a>
         </span>
         <p className={styles.description}>
-            {/* With a passion for developing modern React web apps for commercial
-            bussiness. */}
-            Turning ideas into seamless web experiences with React, I blend creativity and technology to solve business challenges.
+            With a passion for developing modern React web apps for commercial
+            bussiness.
         </p>
 
         
